b38056ab73761091ae684a9dbb33f27b190e2098
Lupau Remus <lupau.remus15@gmail.com>
2023-12-04 10:35:34 +0200

    app done

--- src/components/JokeCard.jsx
+++ src/components/JokeCard.jsx
@@ -4,7 +4,8 @@ const JokeCard = ({ icon_url, joke, getNewJoke }) => {
       className="container-fluid d-flex justify-content-center align-items-center"
       style={{ height: "100vh" }}
     >
-      <div className="text-center">
+      <div className="text-center border border-primary p-5">
+        {/* Unable to fetch the image from the API; it's missing on the server, so leave a comment. */}
         {/* <img src={icon_url} alt="chuck norris png" /> */}
         <p className="h2 my-4">{joke}</p>
         <button
